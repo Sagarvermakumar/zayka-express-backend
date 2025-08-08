aa8ffb87e94790950bfb7237658f2aec2e4a0222
SAGAR VERMA <93580397+Sagarvermakumar@users.noreply.github.com>
2025-08-08 17:49:20 +0530

    Update SendToken.js

--- Utils/SendToken.js
+++ Utils/SendToken.js
@@ -2,20 +2,15 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   const token = user.getJWTToken();
   
 
-  const options = {
-    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    // secure:true,
-    sameSite: "none",
-  };
 
   res
     .status(statusCode)
     .cookie("token", token, {
-      httpOnly: false,
-      secure: true, // Change to true if using HTTPS
-      sameSite: "none", // Use 'none' if frontend & backend are on different domains
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiration
+      httpOnly: true, 
+    secure: true,
+    sameSite: "strict", 
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    path: "/"
     })
     .json({
       success: true,
