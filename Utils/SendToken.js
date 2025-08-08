01ab5e2d0a3adc78ca006e7e7c2fe8415757b523
SAGAR VERMA <93580397+Sagarvermakumar@users.noreply.github.com>
2025-08-08 17:39:36 +0530

    Update SendToken.js

--- Utils/SendToken.js
+++ Utils/SendToken.js
@@ -12,9 +12,9 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   res
     .status(statusCode)
     .cookie("token", token, {
-      httpOnly: true,
-      secure: false, // Change to true if using HTTPS
-      sameSite: "lax", // Use 'none' if frontend & backend are on different domains
+      httpOnly: false,
+      secure: true, // Change to true if using HTTPS
+      sameSite: "none", // Use 'none' if frontend & backend are on different domains
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiration
     })
     .json({
