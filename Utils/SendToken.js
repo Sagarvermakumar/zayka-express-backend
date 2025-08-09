dc1bd3c693259da9c91965cf520d507ebc90d3dc
SAGAR VERMA <93580397+Sagarvermakumar@users.noreply.github.com>
2025-08-09 08:47:48 +0530

    Update SendToken.js

--- Utils/SendToken.js
+++ Utils/SendToken.js
@@ -6,11 +6,11 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   res
     .status(statusCode)
     .cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    path: "/",
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", 
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        path: "/",
   })
     .json({
       success: true,
