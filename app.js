0c62329c15538f7ea14041b20ed163a46ee7225c
SAGAR VERMA <93580397+Sagarvermakumar@users.noreply.github.com>
2025-08-10 09:54:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,17 +21,19 @@ const allowedOrigins = [
 
 app.use(
   cors({
- origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }, methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"], // ✅ FIXED as array
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
-)
+);
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb'  }));
 app.use(cookieParser());
