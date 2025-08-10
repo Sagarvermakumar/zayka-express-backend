118b8bd769fca8febfc473a2761de6d17d23b650
sagarvermakumar <sksagarkumarverma@gmail.com>
2025-08-10 09:38:22 +0530

    updaing cookies option

--- Utils/SendToken.js
+++ Utils/SendToken.js
@@ -1,21 +1,14 @@
 export const sendToken = (res, user, message, statusCode = 200) => {
   const token = user.getJWTToken();
   
-
-  const options = {
-    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    // secure:true,
-    sameSite: "none",
-  };
-
   res
     .status(statusCode)
     .cookie("token", token, {
-      httpOnly: true,
-      secure: false, // Change to true if using HTTPS
-      sameSite: "lax", // Use 'none' if frontend & backend are on different domains
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiration
+      httpOnly: true, 
+      secure: true,
+      sameSite: "strict", 
+      maxAge: 7 * 24 * 60 * 60 * 1000, 
+      path: "/"
     })
     .json({
       success: true,
